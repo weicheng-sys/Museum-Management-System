a5d4a074af9bdb8324ed46b5df4b2d2f540ffdb7
tea-tea-wyr <1623785079@qq.com>
2020-04-27 09:16:48 +0800

    1

--- 一组爬虫/MuseumInformation/MuseumInformation/items.py
+++ 一组爬虫/MuseumInformation/MuseumInformation/items.py
@@ -13,20 +13,32 @@ class MuseuminformationItem(scrapy.Item):
     # name = scrapy.Field()
 
     # 表一
-    museumId = scrapy.Field()
-    museumName = scrapy.Field()
-    museumPosition = scrapy.Field()
-    buildingType = scrapy.Field()
-    ticket = scrapy.Field()
-    buildingTime = scrapy.Field()
-    openingHours = scrapy.Field()
-    collectionNum = scrapy.Field()
-    collectionType = scrapy.Field()
-    number = scrapy.Field()
-    picture = scrapy.Field()
-    VideoAudio = scrapy.Field()
+    # 博物馆编号
+    mid = scrapy.Field()
+    # 博物馆名称
+    mname = scrapy.Field()
+    # 图片
+    mimg = scrapy.Field()
+    # 评价
     evaluate = scrapy.Field()
+    # 评分
     grade = scrapy.Field()
+    # 基本信息
+    basic_info = scrapy.Field()
+    # 其他信息
+    other_info = scrapy.Field()
+    # museumPosition = scrapy.Field()
+    # buildingType = scrapy.Field()
+    # ticket = scrapy.Field()
+    # buildingTime = scrapy.Field()
+    # openingHours = scrapy.Field()
+    # collectionNum = scrapy.Field()
+    # collectionType = scrapy.Field()
+    # number = scrapy.Field()
+    # picture = scrapy.Field()
+    # VideoAudio = scrapy.Field()
+    # evaluate = scrapy.Field()
+    # grade = scrapy.Field()
 
 
 
--- 一组爬虫/MuseumInformation/MuseumInformation/pipelines.py
+++ 一组爬虫/MuseumInformation/MuseumInformation/pipelines.py
@@ -5,20 +5,67 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import json
+import pymysql
+from MuseumInformation.items import MuseuminformationItem
 class MuseuminformationPipeline(object):
     def __init__(self):
         self.fp = open("tableone.json",'w',encoding='utf-8')
     def open_spider(self, spider):
         print("爬虫开始")
+        # self.config = pymysql.connect(
+        #     host='127.0.0.1',
+        #     # port=3306,
+        #     user='museum',
+        #     port=3306,
+        #     passwd='Museum123',
+        #     db='db',
+        #     charset='utf8')
+        # self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
         item_json = json.dumps(dict(item), ensure_ascii=False)
         self.fp.write(item_json+'\n')
         return item
+        # insert_sql = "INSERT INTO hx(mid, mname, ming, evaluate, grade, basic_info, other_info) VALUES (%d, %s, %s, %s, %s, %s, %s)"
+        # self.cursor.execute(insert_sql, (item['mid'], item['mname'], item['ming'], item['evaluate'], item['grade'], item['basic_info'], item['other_info']))
+        # self.client.commit()
+
 
     def close_spider(self, spider):
         self.fp.close()
         print("爬虫结束")
+        # self.cursor.close()
+        # self.connect.close()
+
+
+
+#
+# # 连接配置信息
+# config = {
+#     'host': '127.0.0.1',
+#     'port': 3306,
+#     'user': 'museum',
+#     'password': 'Museum123',
+#     'db': 'db',
+#     'charset': 'utf8',
+#     'cursorclass': pymysql.cursors.DictCursor,
+# }
+#
+# # 创建连接
+# connection = pymysql.connect(**config)
+# # # 创建游标方法1
+# # cursor = connection.cursor()
+# # 创建游标方法2 取别名为 cursor
+# with connection.cursor() as cursor:
+#     # # 执行sql语句，插入记录
+#     # sql = 'INSERT INTO employees (first_name, last_name, hire_date, gender, birth_date) VALUES (%s, %s, %s, %s, %s)'
+#     # cursor.execute(sql, ('Robin', 'Zhyea', tomorrow, 'M', date(1989, 6, 14)))  # 插入一条数据
+#     # data为多条数据，放在一个元组或者列表中
+#     cursor.executemany(sql, data)  # 插入多条数据
+# # 没有设置默认自动提交，需要主动提交，以保存所执行的语句
+# connection.commit()  # 连接提交事务
+# cursor.close()  # 关闭游标连接
+# connection.close();  # 关闭连接，释放内存
 
 import codecs
 # class MuseuminformationPipeline(object):
